628ff420d03176c550f0fe74811a7b009250f34b
n24chris05 <ChrisiMac@Chriss-iMac-5.local>
2014-04-08 00:47:37 +0800

    add hypnoNerd

---
+++ HypnoNerd/BNRAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BNRAppDelegate.h
+//  HypnoNerd
+//
+//  Created by Chris Arquelada on 4/8/14.
+//  Copyright (c) 2014 Big Nerd Ranch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BNRAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
