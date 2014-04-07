f6ad5aa64b38b831eabab7d9b3ae0bd18c5ee0dc
n24chris05 <ChrisiMac@Chriss-iMac-5.local>
2014-04-08 01:33:26 +0800

    //hello world

---
+++ HypnoNerd/BNRHypnosisViewController.h
@@ -0,0 +1,13 @@
+//
+//  BNRHypnosisViewController.h
+//  HypnoNerd
+//
+//  Created by Chris Arquelada on 4/8/14.
+//  Copyright (c) 2014 Big Nerd Ranch. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BNRHypnosisViewController : UIViewController
+
+@end
---
+++ HypnoNerd/BNRHypnosisViewController.m
@@ -0,0 +1,13 @@
+//
+//  BNRHypnosisViewController.m
+//  HypnoNerd
+//
+//  Created by Chris Arquelada on 4/8/14.
+//  Copyright (c) 2014 Big Nerd Ranch. All rights reserved.
+//
+
+#import "BNRHypnosisViewController.h"
+
+@implementation BNRHypnosisViewController
+
+@end
